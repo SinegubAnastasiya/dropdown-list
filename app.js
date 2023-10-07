40cd050abde603a9fbd9b6ce007ab148717eea9e
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-07 13:23:08 +0300

    added dynamic input

--- app.js
+++ app.js
@@ -15,3 +15,30 @@ select.addEventListener('click', () => {
         flag = false;
     }
 })
+
+occupation.addEventListener('click', (event) => {
+    inp.value = event.target.textContent;
+    occupation.style = 'visibility: hidden';
+    arrow.style = 'background-image: url(./arrowdown.svg)';
+    flag = false;
+})
+
+select.addEventListener('dblclick', () => {
+    inp.removeAttribute('disabled');
+    inp.value = '';
+})
+
+inp.addEventListener('keydown', (event) => {
+    try {
+        if (event.key == 'Enter') {
+            if (!inp.value) throw new Error('Input is empty')
+            occupation.style = 'visibility: visible';
+            const p = document.createElement('p');
+            occupation.appendChild(p);
+            p.textContent = inp.value;
+        }
+    } catch (error) {
+        alert(error.message)
+    }
+
+})
